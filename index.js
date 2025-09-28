9a47f0c3e5f16919e48b8914c897d9a2718f0ca3
Chenxi Lin <lin.chenxi14@gmail.com>
2025-09-28 00:09:11 -0400

    Connect to mongo database

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 import express from "express";
 import "dotenv/config";
+import {MongoClient} from "mongodb";
+
+// Connection to MongoDB server
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri);
+const database = client.db("Portfolio");
+
+
+
+
 
 const app = express();
 
 const port = process.env.PORT || "8888";
 
-app.get('/', (request, response) => {
+
+
+app.get('/', async (request, response) => {
     response.send("hello world!");
 })
 
 app.listen(port, () => console.log(`Example app listening on port http://localhost:${port}`));
+
+const viewBlogs = async () => database.collection("blogs").find({}).toArray();
