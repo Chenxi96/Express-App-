1438c5283c207495a93b534aded9d217d9f545a8
Chenxi Lin <lin.chenxi14@gmail.com>
2025-09-29 19:31:56 -0400

    Add view directory

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 import express from "express";
 import "dotenv/config";
-import {MongoClient} from "mongodb";
+import path from 'path';
+import { MongoClient } from "mongodb";
+import { time } from "console";
 
-// Connection to MongoDB server
+// Connection string to MongoDB server
 const uri = process.env.MONGO_URI;
+// Create a connection to mongoDB
 const client = new MongoClient(uri);
+// Connect to mongoDB database named "Portfolio"
 const database = client.db("Portfolio");
 
+// Absolute path
+const __dirname = import.meta.dirname;
 
-
-
-
+// Initialize Express
 const app = express();
+
+// Set view engine with pug
+app.set('view engine', 'pug');
+
+// Direct to the absolute path to the public folder for the static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 const port = process.env.PORT || "8888";
 
-
-
 app.get('/', async (request, response) => {
-    response.send("hello world!");
+    response.render('layout', { name: "Admin"});
 })
 
 app.listen(port, () => console.log(`Example app listening on port http://localhost:${port}`));
